c0c5297c47976fe5c1266c9baab426263217af80
Tambosa <tambosa@gmail.com>
2022-07-05 08:39:35 +0300

    еще тесты для вьюмодели

--- app/src/test/kotlin/TestUserDetailsViewModel.kt
+++ app/src/test/kotlin/TestUserDetailsViewModel.kt
@@ -1,9 +1,12 @@
 import com.aroman.gitandroid.data.web.github.GitServerResponseData
+import com.aroman.gitandroid.data.web.github.GitServerResponseDataOwner
 import com.aroman.gitandroid.domain.usecase.RepositoryUsecase
+import com.aroman.gitandroid.ui.userDetails.UserDetailsFragment
 import com.aroman.gitandroid.ui.userDetails.UserDetailsViewModel
 import com.nhaarman.mockito_kotlin.times
 import com.nhaarman.mockito_kotlin.verify
 import io.reactivex.rxjava3.core.Single
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
@@ -20,6 +23,9 @@ class TestUserDetailsViewModel {
     @Mock
     private lateinit var repositoryLocal: RepositoryUsecase
 
+    @Mock
+    private lateinit var fragment: UserDetailsFragment
+
     @Before
     fun setUp() {
         MockitoAnnotations.initMocks(this)
@@ -33,11 +39,59 @@ class TestUserDetailsViewModel {
     fun searchUserOnlineTest() {
         viewModel.getUser(true, login)
         verify(repositoryWeb, times(1)).getListRepos(login)
+        verify(repositoryLocal, times(0)).getListRepos(login)
     }
 
     @Test
     fun searchUserOfflineTest() {
         viewModel.getUser(false, login)
         verify(repositoryLocal, times(1)).getListRepos(login)
+        verify(repositoryWeb, times(0)).getListRepos(login)
+    }
+
+    @Test
+    fun handleOnlineEmptyResponse() {
+        val response = repositoryWeb.getListRepos(login).blockingGet()
+        assertTrue(response.isEmpty())
+    }
+
+    @Test
+    fun handleOfflineEmptyResponse() {
+        val response = repositoryLocal.getListRepos(login).blockingGet()
+        assertTrue(response.isEmpty())
+    }
+
+    @Test
+    fun handleOnline_ResponseIsNotEmpty() {
+        `when`(repositoryWeb.getListRepos(login)).thenReturn(
+            Single.just(
+                listOf(
+                    GitServerResponseData(
+                        "name",
+                        "url",
+                        GitServerResponseDataOwner("login", "avatarurl")
+                    )
+                )
+            )
+        )
+        val response = repositoryWeb.getListRepos(login).blockingGet()
+        assertTrue(response.isNotEmpty())
+    }
+
+    @Test
+    fun handleOffline_ResponseIsNotEmpty() {
+        `when`(repositoryLocal.getListRepos(login)).thenReturn(
+            Single.just(
+                listOf(
+                    GitServerResponseData(
+                        "name",
+                        "url",
+                        GitServerResponseDataOwner("login", "avatarurl")
+                    )
+                )
+            )
+        )
+        val response = repositoryLocal.getListRepos(login).blockingGet()
+        assertTrue(response.isNotEmpty())
     }
 }
